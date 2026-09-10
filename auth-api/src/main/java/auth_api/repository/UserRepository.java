cfb2aa17d7cda3188855779cd19d4b5672e003b3
yura_sh <ariez_1@ukr.net>
2026-09-10 16:40:58 +0300

    Add User repository

---
+++ auth-api/src/main/java/auth_api/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package auth_api.repository;
+
+import auth_api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
