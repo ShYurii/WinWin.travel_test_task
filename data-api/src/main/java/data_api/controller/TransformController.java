fc6003ded5831edf96ac0f7cd3a750dcc399d06e
yura_sh <ariez_1@ukr.net>
2026-09-10 16:03:41 +0300

    commit 3 Add internal token validation

--- data-api/src/main/java/data_api/controller/TransformController.java
+++ data-api/src/main/java/data_api/controller/TransformController.java
@@ -2,17 +2,26 @@ package data_api.controller;
 
 import data_api.dto.TransformRequest;
 import data_api.dto.TransformResponse;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api")
 public class TransformController {
 
+    @Value("${internal.token}")
+    private String internalToken;
+
     @PostMapping("/transform")
-    public TransformResponse transform(@RequestBody TransformRequest request) {
+    public TransformResponse transform(
+            @RequestHeader(value = "X-Internal-Token", required = false) String token,
+            @RequestBody TransformRequest request) {
+
+        if (!internalToken.equals(token)) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }
 
         String result = request.text().toUpperCase();
 
