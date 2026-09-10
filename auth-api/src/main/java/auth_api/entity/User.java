1c1df877faa13465423261acb710aa6433e84adf
yura_sh <ariez_1@ukr.net>
2026-09-10 16:28:39 +0300

    commit 2 add settings for work with DB Add User entity

---
+++ auth-api/src/main/java/auth_api/entity/User.java
@@ -0,0 +1,42 @@
+package auth_api.entity;
+
+
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    protected User() {
+    }
+
+    public User(String email, String passwordHash) {
+        this.email = email;
+        this.passwordHash = passwordHash;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+}
